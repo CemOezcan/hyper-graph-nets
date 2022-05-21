abe64cd3d42e6856b733202bef4cf7ca7600bd56
Niko <niko.duerr@icloud.com>
2022-05-21 16:00:31 +0000

    Added evaluation method from cloth_eval.py

--- src/tasks/FlagTask.py
+++ src/tasks/FlagTask.py
@@ -8,12 +8,13 @@ import plotly.graph_objects as go
 from src.algorithms.AbstractIterativeAlgorithm import AbstractIterativeAlgorithm
 from src.algorithms.FlagSimulator import FlagModel
 from src.data.dataset import load_dataset
+import common
+
+device = torch.device('cuda')
 
 
 class FlagTask(AbstractTask):
-
-    # TODO: Implement cloth_eval and cloth_plot
-
+    #TODO comments and discussion about nested functions
     def __init__(self, algorithm: AbstractIterativeAlgorithm, config: ConfigDict):
         """
         Initializes all necessary data for a classification task.
@@ -34,6 +35,7 @@ class FlagTask(AbstractTask):
 
         self.classes = self._raw_data.get("classes", None)
         self.out_features = len(self.classes) if self.classes is not None else 1
+        self.mask = None
 
         self._algorithm.initialize(task_information={"input_dimension": self._input_dimension})
 
@@ -53,7 +55,6 @@ class FlagTask(AbstractTask):
 
     def plot(self) -> go.Figure:
         if self._input_dimension == 2:  # 2d classification, allowing for a contour plot
-            import torch
             assert isinstance(self._algorithm, FlagModel)
             points_per_axis = 5
             X = self.raw_data.get("X")
@@ -83,3 +84,52 @@ class FlagTask(AbstractTask):
             return fig
         else:
             raise NotImplementedError("plotting not supported for {}-dimensional features", self._input_dimension)
+
+
+def _rollout(self, model, initial_state, num_steps):
+    """Rolls out a model trajectory."""
+    node_type = initial_state['node_type']
+    self.mask = torch.eq(node_type[:, 0], torch.tensor(
+        [common.NodeType.NORMAL.value], device=device))
+    self.mask = torch.stack((self.mask, self.mask, self.mask), dim=1)
+
+    def step_fn(prev_pos, cur_pos, trajectory):
+        # memory_prev = torch.cuda.memory_allocated(device) / (1024 * 1024)
+        with torch.no_grad():
+            prediction = model({**initial_state,
+                                'prev|world_pos': prev_pos,
+                                'world_pos': cur_pos}, is_training=False)
+
+        next_pos = torch.where(self.mask, torch.squeeze(
+            prediction), torch.squeeze(cur_pos))
+
+        trajectory.append(cur_pos)
+        return cur_pos, next_pos, trajectory
+
+    prev_pos = torch.squeeze(initial_state['prev|world_pos'], 0)
+    cur_pos = torch.squeeze(initial_state['world_pos'], 0)
+    trajectory = []
+    for _ in range(num_steps):
+        prev_pos, cur_pos, trajectory = step_fn(prev_pos, cur_pos, trajectory)
+    return torch.stack(trajectory)
+
+
+def evaluate(self, model, trajectory, num_steps=None):
+    """Performs model rollouts and create stats."""
+    initial_state = {k: torch.squeeze(v, 0)[0] for k, v in trajectory.items()}
+    if num_steps is None:
+        num_steps = trajectory['cells'].shape[0]
+    prediction = self._rollout(model, initial_state, num_steps)
+
+    # error = tf.reduce_mean((prediction - trajectory['world_pos'])**2, axis=-1)
+    # scalars = {'mse_%d_steps' % horizon: tf.reduce_mean(error[1:horizon+1])
+    #            for horizon in [1, 10, 20, 50, 100, 200]}
+
+    scalars = None
+    traj_ops = {
+        'faces': trajectory['cells'],
+        'mesh_pos': trajectory['mesh_pos'],
+        'gt_pos': trajectory['world_pos'],
+        'pred_pos': prediction
+    }
+    return scalars, traj_ops
