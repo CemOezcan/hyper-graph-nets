dedd2d31ef9979218122c98826261f90cbb87aee
Niko <niko.duerr@icloud.com>
2022-05-24 13:48:17 +0000

    Suggestions to change in data_loader.py

--- data/data_loader.py
+++ data/data_loader.py
@@ -1,13 +1,13 @@
 import os
 
-from util.Types import *
+from util.Types import *  # TODO change
 from util.Functions import get_from_nested_dict
 from src.data.dataset import load_dataset
 
 DATA_DIR = os.path.dirname(os.path.abspath(__file__))
 OUT_DIR = os.path.join(DATA_DIR, 'output')
 
-
+# TODO change setup of DataLoader, move to MeshTask maybe?
 def get_data(config: ConfigDict, split='train', split_and_preprocess=True):
     dataset = get_from_nested_dict(config, list_of_keys=["task", "dataset"], raise_error=True)
     if dataset == "two_moons":
