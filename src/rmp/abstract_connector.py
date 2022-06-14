ab4651b3aa6c68ed378c83d47c41b4fb306b29d8
Cem Özcan <cem.oezcan@outlook.com>
2022-06-14 02:26:27 +0200

    Comment abstract_connector.py

--- src/rmp/abstract_connector.py
+++ src/rmp/abstract_connector.py
@@ -1,16 +1,50 @@
 from abc import ABC, abstractmethod
+from typing import List
+
+from src.migration.normalizer import Normalizer
+from src.util import MultiGraphWithPos, MultiGraph
 
 
 class AbstractConnector(ABC):
+    """
+    Abstract superclass for remote message passing strategies.
+    """
 
-    def __init__(self, normalizer):
+    def __init__(self, normalizer: Normalizer):
+        """
+        Initializes the remote message passing strategy.
+
+        Parameters
+        ----------
+        normalizer :  Normalizer for remote edges
+        """
         self._normalizer = normalizer
         self._initialize()
 
     @abstractmethod
     def _initialize(self):
+        """
+        Special initialization function if parameterized preprocessing is necessary.
+
+        Returns
+        -------
+
+        """
         raise NotImplementedError
 
     @abstractmethod
-    def run(self, graph, clusters, is_training):
+    def run(self, graph: MultiGraph, clusters: List[List], is_training: bool) -> MultiGraphWithPos:
+        """
+        Adds remote edges to the input graph.
+
+        Parameters
+        ----------
+        graph :
+        clusters :
+        is_training :
+
+        Returns
+        -------
+
+        """
         raise NotImplementedError
