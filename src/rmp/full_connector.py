ecd2a1548c80357819b676fbd237205c34ab79d5
Cem Özcan <cem.oezcan@outlook.com>
2022-06-17 15:19:00 +0200

    Change multi graph representation in full_connector.py

--- src/rmp/full_connector.py
+++ src/rmp/full_connector.py
@@ -19,12 +19,17 @@ class FullConnector(AbstractConnector):
         pass
 
     def run(self, graph: MultiGraphWithPos, clusters: List[List], is_training: bool) -> MultiGraphWithPos:
-        # Reprs.
-        representatives = []
+        target_feature = graph.target_feature
+        remote_edges = list()
+
+        # Intra cluster communication
+        # TODO: Parameter: num. representatives
+        core_size = 5
+
+        representatives = list()
         for cluster in clusters:
             cluster_size = len(cluster)
-            # TODO: Parameter: num. representatives
-            core_size = 3
+            # TODO: pick based on max node_dynamics or distance from central node (representative)
             random_mask = torch.randperm(n=cluster_size)[0:core_size]
             representatives.append(random_mask)
 
@@ -35,15 +40,64 @@ class FullConnector(AbstractConnector):
             tuples = filter(lambda x: x[0] in indices, enumerate(cluster))
             core_nodes.append(list(map(lambda x: x[1], tuples)))
 
-        # TODO: Decide on whether to create one edge set per cluster or not
-        # TODO: At least separate inter- and intra-cluster edges for num_representatives > 1
+        core_nodes = torch.tensor(core_nodes)
+
+        edges = list()
+        snd = list()
+        rcv = list()
+        for cluster in core_nodes:
+            receivers_list = cluster
+            senders_list = cluster
+            senders = torch.cat(
+                (senders_list.clone().detach(), receivers_list.clone().detach()), dim=0)
+            receivers = torch.cat(
+                (receivers_list.clone().detach(), senders_list.clone().detach()), dim=0)
+            snd.append(senders)
+            rcv.append(receivers)
+
+            # TODO: Make model independent
+            if graph.model_type == 'flag' or graph.model_type == 'deform_model':
+                relative_target_feature = (torch.index_select(input=target_feature, dim=0, index=senders) -
+                                           torch.index_select(input=target_feature, dim=0, index=receivers))
+                edge_features = torch.cat(
+                    (relative_target_feature, torch.norm(relative_target_feature, dim=-1, keepdim=True)), dim=-1)
+            else:
+                raise Exception("Model type is not specified in RippleNodeConnector.")
+
+            edges.append(edge_features)
+
+        edges = self._normalizer(torch.cat(edges, dim=0))
+        snd = torch.cat(snd, dim=0)
+        rcv = torch.cat(rcv, dim=0)
+        world_edges = EdgeSet(
+            name='remote_edges',
+            features=self._normalizer(edges, None, is_training),
+            receivers=rcv,
+            senders=snd)
+
+        remote_edges.append(world_edges)
+
+        # Inter cluster communication
+        core_size = 1
+
+        representatives = list()
+        for cluster in clusters:
+            cluster_size = len(cluster)
+            # TODO: pick based on max node_dynamics or distance from central node (representative)
+            random_mask = torch.randperm(n=cluster_size)[0:core_size]
+            representatives.append(random_mask)
+
+        representatives = [x.tolist() for x in representatives]
+
+        core_nodes = list()
+        for indices, cluster in zip(representatives, clusters):
+            tuples = filter(lambda x: x[0] in indices, enumerate(cluster))
+            core_nodes.append(list(map(lambda x: x[1], tuples)))
+
         core_nodes = torch.tensor(sum(core_nodes, list()))
-
-        target_feature = graph.target_feature
-        remote_edges = []
-
         receivers_list = core_nodes
         senders_list = core_nodes
+
         senders = torch.cat(
             (senders_list.clone().detach(), receivers_list.clone().detach()), dim=0)
         receivers = torch.cat(
@@ -66,6 +120,8 @@ class FullConnector(AbstractConnector):
             senders=senders)
 
         remote_edges.append(world_edges)
+
+        # Expansion
         edge_sets = graph.edge_sets
         edge_sets.extend(remote_edges)
 
