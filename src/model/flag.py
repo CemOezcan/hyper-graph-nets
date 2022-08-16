01928f7d7c1f86a89e04cca910bcddf37df9838a
Cem Özcan <cem.oezcan@outlook.com>
2022-08-16 23:33:28 +0200

    Refactoring.

--- src/model/flag.py
+++ src/model/flag.py
@@ -10,7 +10,7 @@ from src.rmp.remote_message_passing import RemoteMessagePassing
 from src.migration.normalizer import Normalizer
 from src.migration.meshgraphnet import MeshGraphNet
 from src import util
-#from src.rmp.ricci import Ricci
+from src.rmp.ricci import Ricci
 from src.util import NodeType, EdgeSet, MultiGraph, device, MultiGraphWithPos
 
 
@@ -39,9 +39,9 @@ class FlagModel(nn.Module):
         self.message_passing_aggregator = params.get('aggregation')
 
         self._edge_sets = ['mesh_edges']
-    #    if self._ricci:
-     #       self._ricci_flow = Ricci()
-      #      self._edge_sets.append('ricci')
+        if self._ricci:
+            self._ricci_flow = Ricci()
+            self._edge_sets.append('ricci')
         if self._rmp:
             self._remote_graph = rmp.get_rmp(params)
             self._edge_sets += self._remote_graph.initialize(self._intra_edge_normalizer, self._inter_edge_normalizer)
