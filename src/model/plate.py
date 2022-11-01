cda2567177d8a806ae00d3b2f943a2805debfc55
Cem Özcan <cem.oezcan@outlook.com>
2022-11-02 00:30:52 +0100

    Include world edges.

--- src/model/plate.py
+++ src/model/plate.py
@@ -43,7 +43,7 @@ class PlateModel(AbstractSystemModel):
         self._rmp_frequency = params.get('rmp').get('frequency')
         self._visualized = False
 
-        self._edge_sets = ['mesh_edges']
+        self._edge_sets = ['mesh_edges', 'world_edges']
         if self._balancer:
             import src.graph_balancer.get_graph_balancer as graph_balancer
             self._graph_balancer = graph_balancer.get_balancer(params)
