85b9fd9618401bb9314c16bb597ade96bff85eff
Cem Özcan <cem.oezcan@outlook.com>
2022-11-26 01:18:55 +0100

    Bugfixing.

--- src/rmp/remote_message_passing.py
+++ src/rmp/remote_message_passing.py
@@ -127,7 +127,7 @@ class RemoteMessagePassing:
         if fst == 0:
             for i in range(len(self._clusters)):
                 print(self._clusters[i].get_device())
-                tensor = torch.tensor([lst + 1] * len(self._clusters[i])).to(device)
+                tensor = torch.tensor([lst + 1] * len(self._clusters[i])).to('cpu')
                 self._clusters[i] = torch.add(tensor, self._clusters[i])
 
     def reset_clusters(self):
